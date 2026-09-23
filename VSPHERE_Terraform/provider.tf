4c33fde721fe0bfd30d2b00bf05cacba68728b48
venatisaipavan <venatisaipavan@gmail.com>
2026-09-23 19:49:50 +0530

    Terraform vsphere folder created by sai

---
+++ VSPHERE_Terraform/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "vmware/vsphere"
+      version = "2.17.1"
+    }
+  }
+}
+
+provider "vsphere" {
+  # Configuration options
+}
